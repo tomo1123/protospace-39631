6f99e9a9a13e7530297b6c33cc88d53cfd6640e5
tomo1123 <tomocruise01@gmail.com>
2023-10-06 19:06:01 +0900

    コンテントに修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ApplicationRecord
 
   belongs_to :user
   belongs_to :prototype
-  
-  validates :comment, presence: true
+ 
+  validates :content, presence: true
   #validates :user, presence: true
   #validates :prototype, presence: true
 
